8f6642fc55d6746068e57564fc838d8d3a6d4814
xiejingyao <448184641@qq.com>
2018-02-23 17:33:09 +0800

    text component

--- src/LayaSample.ts
+++ src/LayaSample.ts
@@ -2,7 +2,26 @@
 class GameMain{
     constructor()
     {
-        Laya.init(600,400);
+        //初始化引擎
+        Laya.init(1136,640);
+        var txt:Laya.Text = new Laya.Text();
+        //设置文本内容
+        txt.text = "hello_world";
+        //设置文本颜色
+        txt.color = "#ffffff";
+        //设置文本字体
+        txt.font = "Ya Hei";
+        //设置字体大小
+        txt.fontSize = 32;
+        //设置文本取背景
+        txt.bgColor = "#c30c30";
+        //设置文本框的颜色
+        txt.borderColor = "#23cfcf";
+        //设置粗体、斜体
+        txt.bold = true;
+        //设置斜体
+        txt.italic = true;
+        Laya.stage.addChild(txt);
     }
 }
 new GameMain();
